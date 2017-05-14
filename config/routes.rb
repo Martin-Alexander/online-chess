551d132a40d4185eeb8ab9bdacaaf2ac912ed60e
Martin Giannakopoulos <martingianna@gmail.com>
2017-05-14 01:51:57 -0400

    preparing routes and controllers for implimentation of multipe multiplayer chess games

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "game#board", as: "board"
+  root to: "game#home", as: "home"
+
+  get "new", to: "game#new", as: "new"
+  post "create", to: "game#create", as: "create"
+
+  get "show/:game_id", to: "game#show", as: "show"
   get "data", to: "game#data", as: "data"
+  
   post "move", to: "game#move", as: "move"
+  
   get "reset", to: "game#reset", as: "reset"
 
   mount ActionCable.server, at: '/cable'
