c8168bce94e9915deda32f4b1511f5686ead6246
Martin Giannakopoulos <martingianna@gmail.com>
2017-05-17 02:04:18 -0400

    indendation

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Array.send(:include, SerializeBoard)
 String.send(:include, ParseBoard)
 
 def each_square
-(0..7).each do |rank|
-  (0..7).each do |file|
-    yield(rank, file)
-  end
-end
+	(0..7).each do |rank|
+	  (0..7).each do |file|
+	    yield(rank, file)
+	  end
+	end
 end
 
 Rails.application.initialize!
