45ed9ba139af921f01388cd1246714a68ab92572
Martin Giannakopoulos <martingianna@gmail.com>
2017-05-22 03:34:30 -0400

    improved position evaluation and added color independent engine calculation

--- app/jobs/engine_thought.rb
+++ app/jobs/engine_thought.rb
@@ -24,33 +24,33 @@ class EngineThought < ApplicationJob
 		[0, 0, 0, 0, 0, 0, 0, 0],
 		[0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
 		[0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
-		[0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
-		[0.1, 0.1, 0.2, 0.5, 0.5, 0.2, 0.1, 0.1],
-		[0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0.1, 0.1],
-		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0.2, 0.2, 0.2, 0.4, 0.4, 0.2, 0.2, 0.2],
+		[0.1, 0.1, 0.1, 0.5, 0.5, 0.1, 0.1, 0.1],
+		[0.0, 0.0, 0.0, 0.3, 0.3, 0.0, 0.0, 0.0],
+		[0, 0, 0, -0.2, -0.2, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0]
 	]
 
 	KNIGHT_POS_EVAL = [
-		[-0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4],
-		[-0.4, 0, 0, 0, 0, 0, 0, -0.4],
-		[-0.4, 0, 0.2, 0.2, 0.2, 0.2, 0, -0.4],
-		[-0.4, 0, 0.2, 0.4, 0.4, 0.2, 0, -0.4],
-		[-0.4, 0, 0.2, 0.4, 0.4, 0.2, 0, -0.4],
-		[-0.4, 0, 0.2, 0.2, 0.2, 0.2, 0, -0.4],
-		[-0.4, 0, 0, 0, 0, 0, 0, -0.4],
-		[-0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4]
+		[-0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2],
+		[-0.2, 0, 0, 0, 0, 0, 0, -0.2],
+		[-0.2, 0, 0.1, 0.1, 0.1, 0.1, 0, -0.2],
+		[-0.2, 0, 0.1, 0.1, 0.1, 0.1, 0, -0.2],
+		[-0.2, 0, 0.1, 0.1, 0.1, 0.1, 0, -0.2],
+		[-0.2, 0, 0.2, 0.1, 0.1, 0.2, 0, -0.2],
+		[-0.2, 0, 0, 0, 0, 0, 0, -0.2],
+		[-0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2]
 	]
 
 	BISHOP_POS_EVAL = [
 		[0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0.2, 0, 0, 0, 0, 0.2, 0],
-		[0, 0, 0.2, 0, 0, 0.2, 0, 0],
-		[0, 0, 0, 0.2, 0.2, 0, 0, 0],
-		[0, 0, 0, 0.2, 0.2, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0]
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, -0.1, 0, 0, -0.1, 0, 0]
 	]
 
 	ROOK_POS_EVAL = [
@@ -83,7 +83,7 @@ class EngineThought < ApplicationJob
 		[0, 0, 0, 0, 0, 0, 0, 0],
 		[-0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4, -0.4],
 		[-0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2],
-		[0.2, 0.2, 0.8, 0.2, 0.2, 0.2, 0.8, 0.2]
+		[0.2, 0.2, 0.3, 0.2, 0.2, 0.2, 0.3, 0.2]
 	]
 
 	PIECE_VALUE = {
@@ -128,11 +128,7 @@ class EngineThought < ApplicationJob
 
   def mamaburger(initial_board, current_game)
   	move_evaluations = tree_evaluator(deep_thought(initial_board))
-  	best_move_index = move_evaluations.each_with_index.min[1]
-  	worst_move_index = move_evaluations.each_with_index.max[1]
-		# initial_board.moves.each_with_index { |e, i| puts "#{e.to_s}: #{move_evaluations[i]}" }
-  	# puts "best: #{initial_board.moves[best_move_index].to_s}"
-  	# puts "worst: #{initial_board.moves[worst_move_index].to_s}"
+  	best_move_index = move_evaluations.each_with_index.max[1]
   	computer_move_board = initial_board.move(initial_board.moves[best_move_index])
   	computer_move_board.game = current_game
     computer_move_board.save!
@@ -156,33 +152,6 @@ class EngineThought < ApplicationJob
 	  return total_material_score
   end
 
-  # def deep_thought(board_object)
-  # 	start_time = Time.now
-  #   tree = []
-  #   board_object.moves.each_with_index do |move_one, i|
-  #   	puts "#{i / (board_object.moves.length * 1.00)}" 
-  #     branch_one = []
-  #     tree << branch_one
-  #     first_level_board = board_object.computer_move(move_one)
-  #     first_empty = first_level_board.moves.each do |move_two|
-  #       branch_two = []
-  #       branch_one << branch_two
-  #       second_level_board = first_level_board.computer_move(move_two)
-  #       second_empty = second_level_board.moves.each do |move_three|
-  #         third_level_board = second_level_board.computer_move(move_three)
-  #         evaluation = static_board_evaluation(third_level_board.board_data.to_board)
-  #         branch_two << evaluation
-  #       end.empty?
-  #       branch_one << [999] if second_empty
-  #     end.empty?
-  #     tree << [[-999]] if first_empty
-  #   end
-  #   end_time = Time.now
-  #   puts "#{tree.flatten.length} boards evaluated in #{(end_time - start_time) / 60.0} minutes"
-  #   tree.flatten.any? { |i| i.nil? } ? print("there is a nil") : print("there is not a nil")
-  #   return tree
-  # end
-
 	def deep_thought(board_object)
   	start_time = Time.now
 	  tree = []
@@ -192,7 +161,7 @@ class EngineThought < ApplicationJob
       first_level_board = board_object.computer_move(move_one)
       first_level_board_moves = first_level_board.moves
 	    if first_level_board_moves.empty?
-	      tree << [[-999]]
+	      tree << [[999]]
 	    else
 	      tree << branch_one
 	      first_level_board_moves.each do |move_two|
@@ -200,12 +169,13 @@ class EngineThought < ApplicationJob
 	        second_level_board = first_level_board.computer_move(move_two)
 	        second_level_board_moves = second_level_board.moves
 	        if second_level_board_moves.empty?
-            branch_one << [999]
+            branch_one << [-999]
 	        else
 	          branch_one << branch_two
 	          second_level_board_moves.each do |move_three|
 	            third_level_board = second_level_board.computer_move(move_three)
-              branch_two << static_board_evaluation(third_level_board.board_data.to_board)
+              board_eval = static_board_evaluation(third_level_board.board_data.to_board)
+							board_object.white_to_move ? branch_two <<  board_eval : branch_two << 0 - board_eval
 	          end
 	        end
 	      end
@@ -220,9 +190,9 @@ class EngineThought < ApplicationJob
 	def tree_evaluator_helper(list, level)
 	  if list.all? { |i| i.kind_of?(Array) }
 	    if level % 2 == 0
-	      return(list.map { |j| (tree_evaluator_helper(j, level + 1)).min })
-	    else
 	      return(list.map { |j| (tree_evaluator_helper(j, level + 1)).max })
+	    else
+	      return(list.map { |j| (tree_evaluator_helper(j, level + 1)).min })
 	    end
 	  else
 	    return(list)  
