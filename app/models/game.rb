c953404e4721432192d67ead36a3819564f171c0
Martin Giannakopoulos <martingianna@gmail.com>
2017-05-25 16:29:17 -0400

    helper functions for Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@ class Game < ApplicationRecord
   belongs_to :black, class_name: "User"
   belongs_to :white, class_name: "User"
   has_many :boards, dependent: :destroy
+
+  def unfull?
+  	white.nil? || black.nil?
+  end
+
+  def game_over?
+		self.boards.last.moves.zero?  	
+  end
 end
