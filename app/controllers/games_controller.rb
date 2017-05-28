c846721235bd03e57bb5eaee4ec10d4f3310be0d
Martin Giannakopoulos <martingianna@gmail.com>
2017-05-28 16:09:00 -0400

    refactoring and commenting the game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,51 +1,40 @@
 class GamesController < ApplicationController
 
   def new
+    # Rendering new single player game screen wherein you choose which AI to play against
   end
 
   def create
-    if params[:ai]
-      if params[:color] == "choose-white"
-        new_game = Game.create(
-          white: current_user ? current_user : User.find_by(email: "guest"),
-          black: User.find_by(email: params[:ai])
-        )
+    create_game = lambda { |white, black| Game.create(white: white, black: black) }
+
+    if params[:ai] # Single player game
+      new_game = if params[:color] == "choose-white"
+        create_game.call(current_user, User.find_by(email: params[:ai]))
       else
-        new_game = Game.create(
-          white: User.find_by(email: params[:ai]),
-          black: current_user ? current_user : User.find_by(email: "guest")
-        )
+        create_game.call(User.find_by(email: params[:ai]), current_user)
       end
-    else
+    else # Multiplayer game
       host = User.find(params[:lobby_host])
       nonhost = User.find(params[:nonhost])
-      if params[:host_is] == "random"
-        if rand > 0.5
-          new_game = Game.create(white: host, black: nonhost)
-        else
-          new_game = Game.create(white: nonhost, black: host)
-        end
+      new_game = if params[:host_is] == "random"
+        rand > 0.5 ? create_game.call(host, nonhost) : create_game.call(nonhost, host)
       elsif params[:host_is] == "white"
-        new_game = Game.create(white: host, black: nonhost)
+        create_game.call(host, nonhost)
       else
-        new_game = Game.create(white: nonhost, black: host)
+        create_game.call(nonhost, host)
       end
     end
-    Board.create(
-      game: new_game,
-      ply: 1,
-      board_data: "-4,-2,-3,-5,-6,-3,-2,-4,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,4,2,3,5,6,3,2,4",
-      white_to_move: true,
-      castling: "1111",
-      en_passant: "1111"
-    )
+
+    Board.create(game: new_game)
     redirect_to game_path(new_game)
   end
 
   def show
     @game = Game.find(params[:id])
     @board = @game.boards.last
-    if @board.turn_player && !@board.turn_player.human && @board.ply == 1
+
+    if @board.turn_player && !@board.turn_player.human && @board.ply == 1 
+      # Automatically run EngineThought if it's the first ply and the turnplayer is a computer
       EngineThought.perform_later(@board.id, @game.id, @board.turn_player.email)
     end
   end
