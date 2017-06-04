93b99966af07b50aaa21177c2107c14bf8f40c41
Martin Giannakopoulos <martingianna@gmail.com>
2017-06-04 00:36:49 -0400

    304 page status also passes in addition to 200

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class HomeTest < ActionDispatch::IntegrationTest
   test "page loads correctly" do
     visit "/"
-    assert_equal 200, page.status_code
+    if page.status_code == 304
+    	assert_equal 304, page.status_code
+    else
+	    assert_equal 200, page.status_code
+    end
   end
 end
--- test/integration/new_multiplayer_game_test.rb
+++ test/integration/new_multiplayer_game_test.rb
@@ -4,6 +4,10 @@ class NewMultiplayerGameTest < ActionDispatch::IntegrationTest
   test "page correctly" do
   	login_as users(:martin)
     visit "/lobbies/new"
-    assert_equal 200, page.status_code
+    if page.status_code == 304
+    	assert_equal 304, page.status_code
+    else
+	    assert_equal 200, page.status_code
+    end
   end
 end
