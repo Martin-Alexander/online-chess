248425e4bab8dbedc87ca7d03628320f00090cdb
Martin Giannakopoulos <martingianna@gmail.com>
2017-06-07 11:28:48 -0400

    moved first board creation from Game controller to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,13 @@ class Game < ApplicationRecord
   belongs_to :black, class_name: "User", optional: true
   belongs_to :white, class_name: "User", optional: true
   has_many :boards, dependent: :destroy
+  after_create :first_board
 
   def game_over?
 		self.boards.last.moves.zero?  	
+  end
+
+  def first_board
+  	Board.create(game: self)
   end
 end
