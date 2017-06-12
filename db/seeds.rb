784127e7a069a2d7c70664c7303518a876278998
Martin Giannakopoulos <martingianna@gmail.com>
2017-06-12 13:17:49 -0400

    added papaburger and grandpaburger to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@ User.create(email: "player_two", password: "123456", human: true)
 User.create(email: "babyburger", password: "qwerty", human: false)
 User.create(email: "teenburger", password: "qwerty", human: false)
 User.create(email: "mamaburger", password: "qwerty", human: false)
+User.create(email: "papaburger", password: "qwerty", human: false)
+User.create(email: "grandpaburger", password: "qwerty", human: false)
 
